0619ce62dc6e875a1da82ac9830dd31aaf1cf2ef
Anoop AG <anoop.gunaga@robosoftin.com>
2016-12-01 09:44:23 +0530

    AGFramework v1.0.0

---
+++ Builds/AGFramework/AGFramework.framework/Headers/AGFraeworkConstants.h
@@ -0,0 +1,24 @@
+//
+//  AGFraeworkConstants.h
+//  AGFramework
+//
+//  Created by Anoop Gunaga on 28/11/16.
+//  Copyright © 2016 Robosoft. All rights reserved.
+//
+
+#ifndef AGFraeworkConstants_h
+#define AGFraeworkConstants_h
+
+#pragma mark - MACROS
+#pragma mark - 
+
+#pragma mark - ENUMS
+
+typedef NS_ENUM(NSInteger, AGFrameworkLogLevel) {
+    eAGFrameworkLogLevelAll = 1,
+    eAGFrameworkLogLevelMinimal,
+    eAGFrameworkLogLevelError,
+};
+
+
+#endif /* AGFraeworkConstants_h */
