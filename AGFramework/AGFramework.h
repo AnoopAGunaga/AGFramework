34a534df5e3575594ac7e9b4e4d335c834d5ccf6
Anoop Athmaram Gunaga <anoopag@iMac.local>
2017-10-12 15:44:58 +0530

    Added new method

--- AGFramework/AGFramework.h
+++ AGFramework/AGFramework.h
@@ -26,6 +26,7 @@ FOUNDATION_EXPORT const unsigned char AGFrameworkVersionString[];
  *  @param level SDK log level
  */
 + (void) initialise:(NSString *)key logLevel:(AGFrameworkLogLevel)level;
++ (void) initialise;
 
 
 /*!
